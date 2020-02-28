514fa9d02b466b0c2d98a0bd1805e21483b90408
thir13en <santilpzate@gmail.com>
2020-02-28 19:40:25 +0100

    added first process fork

--- docs/PROCESSES.md
+++ docs/PROCESSES.md
@@ -2,3 +2,11 @@
 
 
 In Unix there is a hierarchy of processes ruled by an initial process, from which all the processes are generated, this process, the `init` process, has a `pid` of 1 and is of vital importance in the system lifecycle.
+
+## Important info about a process
+* Process ID -> PID
+* parent process ID
+* process group ID
+* user id -> UID
+* user group id -> GID
+* State of the process
---
+++ first-process-fork.c
@@ -0,0 +1,24 @@
+main() {
+	int process, st, pid;
+	char s[80];
+
+	switch (process = fork()) {
+
+	case -1:
+		sprintf(s, "There was an error in the fork\n");
+		write(2, s, strlen(s));
+		exit(1);
+
+	case 0:
+		sprintf(s, "Whaaaa!! You just had a child!!!\n");
+		write(1, s, strlen(s));
+		exit(0);
+
+	default:
+		sprintf(s, "I am the father process and my child is: %d\n", process);
+		write(1, s, strlen(s));
+		pid = wait(&st);
+		exit(0);
+
+	}
+}
