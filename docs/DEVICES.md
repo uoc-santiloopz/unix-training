15295e9b457bb2797a7562092fdc5c7d51fa0ee7
thir13en <santilpzate@gmail.com>
2020-03-02 23:18:47 +0100

    created data reading program

---
+++ docs/DEVICES.md
@@ -0,0 +1,6 @@
+# DEVICES
+
+In Unix based OSs, every device is represented through a file named `file descriptor`, which occupies a channel in the I/O estrategy of the OS. By consensus:
+* 0 channel is the stdin
+* 1 channel is the stout
+* 2 channel is the stderr
---
+++ playing-with-devices.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h> // definicions O_RDONLY, O_WRONLY, ...
+#include <errno.h>
+#include <string.h>
+
+
+void error(char * m) {
+	write(2, m, strlen(m));
+	write(2, "\n", 1);
+	write(2, strerror(errno), strlen(strerror(errno)));
+	exit(1);
+}
+
+int main() {
+	int fd, n;
+	char c;
+
+	if ((fd = open("../data/datafile.dat", O_RDONLY)) < 0) {
+		error("Opening file");
+		// reads while there is info and writes throught the stdout
+		while ((n=read(fd, &c, 1)) > 0) {
+			write(1, &c, 1);
+			if (n < 0) {
+				// finished reading file
+				error("file finished reading");
+				close (fd);
+			}
+		}
+	}
+}
